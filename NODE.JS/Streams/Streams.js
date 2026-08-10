c5106ac39f3482c6cc65fb687cff87f997ecf3b4
Aqdas <habibaqdas2019@gmail.com>
2026-08-10 23:57:23 +0530

    modification on projects

--- NODE.JS/Streams/Streams.js
+++ NODE.JS/Streams/Streams.js
@@ -1,6 +1,9 @@
 const http = require("http");
 const fs = require("fs");
 const Port = 8080;
+const path = require("path");
+const read = path.join(__dirname,"Sample.txt");
+const write = path.join(__dirname,"Output.txt");
 
 const server = http.createServer((req,res)=>{
     //? <----1----->
@@ -12,9 +15,10 @@ const server = http.createServer((req,res)=>{
     //const readableStream = fs.createReadStream("Sample.txt");
     //readableStream.pipe(res);
 
-    //? <-----2----->
-    const readable = fs.createReadStream("Sample.txt");
-    const writeable = fs.createWriteStream("Output.txt");
+    //? <-----2-----> Creating and Reading a file using streams
+
+    const readable = fs.createReadStream(read);
+    const writeable = fs.createWriteStream(write);
     readable.pipe(writeable); // copy Sample.txt --> Output.txt
 
     readable.on("data", (chunk) => {
@@ -27,6 +31,13 @@ const server = http.createServer((req,res)=>{
     readable.on("error", (err) => {
         res.end("Error: " + err.message);
     });
+     
+    //? <-----3-----> TRANSFORM STREAMS
+    //task-- uppercase() , impulse -> Aqdas
+    readable.on("data", (chunk) => {
+        const modifiedChunk = chunk.toString().toUpperCase().replaceAll(/ipsum/gi, "Aqdas");
+        writeable.write(modifiedChunk);
+    });
 
 
 });
@@ -34,6 +45,7 @@ const server = http.createServer((req,res)=>{
 //  readable<--- pipe --->writeable 
 // res : readablestream
 // req : writeablestream
+
 
 
 
